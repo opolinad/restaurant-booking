0bf07c0114259edb54ea0712bbb576dd77dcd8a2
Daniel Andrés López Sabogal <andreslop@gmail.com>
2023-03-18 18:31:38 -0500

    add migrations

---
+++ restaurant_booking/main/migrations/0002_auto_20230318_1747_seed_database.py
@@ -0,0 +1,32 @@
+from django.db import migrations
+from csv import DictReader
+from main.models import Restaurant
+
+
+def seed_database(apps, schema_editor):
+    for row in DictReader(open('D:/Programación/Retos/restaurant-booking/restaurantes.csv', encoding="utf8")):
+        restaurant = Restaurant(
+            id = row['id'],
+            rating = row['rating'],
+            name = row['name'],
+            site = row['site'],
+            email = row['email'],
+            phone = row['phone'],
+            street = row['street'],
+            city = row['city'],
+            state = row['state'],
+            lat = row['lat'],
+            lng = row['lng']
+        )
+        restaurant.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('main', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(seed_database)
+    ]
