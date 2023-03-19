46c9c55e6607d3a526601018595daa923fe0ddd9
Daniel Andrés López Sabogal <andreslop@gmail.com>
2023-03-19 18:21:54 -0500

    add model

--- restaurant_booking/main/models.py
+++ restaurant_booking/main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator, URLValidator, validate_email
 
-# Create your models here.
+class Restaurant(models.Model):
+    id = models.TextField(primary_key=True)
+    rating = models.IntegerField(validators=[
+        MaxValueValidator(4),
+        MinValueValidator(0)
+    ])
+    name = models.TextField()
+    site = models.TextField(validators=[URLValidator])
+    email =  models.TextField(validators=[validate_email])
+    phone = models.TextField()
+    street = models.TextField()
+    city = models.TextField()
+    state = models.TextField()
+    lat = models.FloatField()
+    lng = models.FloatField()
